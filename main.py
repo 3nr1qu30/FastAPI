7e5f9cff0c6e0c96adf8558c4eed5f65afb63386
3nr1qu30 <evarillasf2000@alumno.ipn.mx>
2023-05-25 18:28:49 -0600

    Prueba de la api 1

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
+import base64
+import wave
+from starlette.responses import FileResponse
+from starlette import status
 
-app = FastAPI()
+app= FastAPI()
 
-class Msg(BaseModel):
-    msg: str
+# Configurar los orígenes permitidos en CORS
+origins = [
+    "http://localhost:8080",
+    # Agrega aquí los demás orígenes permitidos
+]
 
+# Agregar el middleware de CORS a la aplicación
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
+class AudioData(BaseModel):
+    audioBase64: str
 
+@app.post("/")
+async def index(audio_data: AudioData):
+    audio_base64 = audio_data.audioBase64
+    print(audio_base64)
 
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
+    # Decodificar la cadena base64
+    audio_decoded = base64.b64decode(audio_base64)
+    print(audio_decoded)
 
+    # Guardar el audio decodificado en un archivo WAV
+    with wave.open("audio.wav", "w") as wav_file:
+        wav_file.setnchannels(1)  # Configurar el número de canales (mono)
+        wav_file.setsampwidth(2)  # Configurar el ancho de muestra en bytes (2 bytes para 16 bits)
+        wav_file.setframerate(44100)  # Configurar la frecuencia de muestreo (44.1 kHz)
+        wav_file.writeframes(audio_decoded)
 
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
-
-
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+    # Devolver la respuesta con el archivo de audio descargable
+    return FileResponse("audio.wav", filename="audio.wav", media_type="audio/wav", status_code=status.HTTP_200_OK)
