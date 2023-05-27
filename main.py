30e484dff3dae12eb52dd7f5477e63fa9da57205
3nr1qu30 <evarillasf2000@alumno.ipn.mx>
2023-05-27 00:16:36 -0600

    yo esa mami

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ async def index(data: TranscriptionData):
 
     openai.api_key = os.getenv("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")
 
-    response = openai.Completion.create(
+    response =await openai.Completion.create(
     model="text-davinci-003",
     prompt=transcription + "\n\nTl;dr",
     temperature=0.7,
